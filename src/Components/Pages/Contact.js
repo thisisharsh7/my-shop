fa82e66111dffc9820df8bc6f84c2c362365f883
Harsh Kumar <9u.harsh@gmail.com>
2022-10-19 09:32:44 +0530

    contact design end

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -4,29 +4,29 @@ import phone from '../Images/phone.png';
 import locat from '../Images/locat.png';
 import msg from '../Images/msg.png';
 
-export default function Contact(){
-    return(
-        <section>
-        <div>
-            <div>
-                <h1>Get In Touch</h1>
-                <p>We are here for you! How can we help?</p>
+export default function Contact() {
+    return (
+        <section className='contact'>
+            <div className='contact-form'>
+                <div className='contact-text'>
+                    <h1>Get In Touch</h1>
+                    <p>We are here for you! How can we help?</p>
+                </div>
+                <form>
+                    <input type="text" name="username" placeholder="Enter your name" />
+                    <input type="email" name="email" placeholder="Enter your email address" />
+                    <textarea name="message" placeholder="Send us a message" />
+                    <input type="submit" name="submit" />
+                </form>
             </div>
-            <form>
-                <input type="text" name="username" placeholder="Enter your name" />
-                <input type="email" name="email" placeholder="Enter your email address" />
-                <input name="message" placeholder="Send us a message" />
-                <input type="submit" name="submit" />
-            </form>
-        </div>
-        <div>
-            <img src={phoneBg} alt="" />
-            <div>
-                <p><span><img src={locat} alt="" /></span><span>Sector-9, Dwarka, New Delhi</span></p>
-                <p><span><img src={msg} alt="" /></span><span>admin@welcome-myshop.com</span></p>
-                <p><span><img src={phone} alt="" /></span><span>9310845435</span></p>
+            <div className='contactImg'>
+                <img src={phoneBg} alt="" />
+                <div>
+                    <p><span><img src={locat} alt="" /></span><span>Sector-9, Dwarka, New Delhi</span></p>
+                    <p><span><img src={msg} alt="" /></span><span>admin@welcome-myshop.com</span></p>
+                    <p><span><img src={phone} alt="" /></span><span>9310845435</span></p>
+                </div>
             </div>
-        </div>
 
         </section>
     )
