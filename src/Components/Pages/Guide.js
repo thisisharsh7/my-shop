d2e5fc64cf04707dcdd4192adf30d0d703722aea
Harsh Kumar <9u.harsh@gmail.com>
2022-10-19 13:34:49 +0530

    half guide page done

--- src/Components/Pages/Guide.js
+++ src/Components/Pages/Guide.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import storeStats from '../Images/storeStats.png';
+import one from '../Images/Guide/one.png';
 
 export default function Guide(){
     return(
-        <section>
-            <h1>This is Guide Section</h1>
+        <section className='guide'>
+            <div className='guide-bg'>
+              <div className='guide-intro'>
+                <h1>Quick <span>guide</span> for our <span>seller</span> army</h1>
+                <img src={storeStats} alt="" />
+              </div>
+              <div className='fb'>
+                <h1>Join The Seller's Community On Facebook</h1>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">Join Now</a>
+              </div>
+            
+            </div>
         </section>
     )
 }
