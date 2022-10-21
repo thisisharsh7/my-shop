4ee5844163a91d33f8fb2399bfc38338611f9ddd
Harsh Kumar <9u.harsh@gmail.com>
2022-10-21 20:03:26 +0530

    animation package added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
-import Home from "./Components/Pages/Home";
+import AnimatedRoutes from "./Components/Pages/AnimatedRoutes";
 import Footer from "./Components/Footer/Footer";
-import Terms from "./Components/Pages/Terms";
-import Policy from "./Components/Pages/Policy";
-import Faq from "./Components/Pages/Faq";
-import Guide from "./Components/Pages/Guide";
-import Contact from "./Components/Pages/Contact";
-import { Route, Routes } from "react-router-dom";
+
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/policy" element={<Policy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/guide" element={<Guide />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AnimatedRoutes />
       <Footer />
     </>
   );
---
+++ src/Components/Pages/AnimatedRoutes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Terms from "./Terms";
+import Policy from "./Policy";
+import Faq from "./Faq";
+import Guide from "./Guide";
+import Contact from "./Contact";
+import Home from "./Home";
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+export default function AnimatedRoutes() {
+    const location = useLocation();
+    return (
+        <AnimatePresence>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/policy" element={<Policy />} />
+                <Route path="/terms" element={<Terms />} />
+                <Route path="/guide" element={<Guide />} />
+                <Route path="/faq" element={<Faq />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </AnimatePresence>
+    )
+}
