fa552c213ba353a5393348990ea5b6c1e53e90da
Harsh Kumar <9u.harsh@gmail.com>
2022-10-23 21:45:07 +0530

    some sizing part done

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -29,43 +29,41 @@ export default function Hero() {
   })
   return (
     <>
-      <section>
-        <div className="hero-bg">
-          <div className="hero">
-            <div className="text-hero">
-              <div className="head-hero">
-                <h1>
-                  Search online buy</h1>
-                <div className="relative-text">
-                  <h1 className="offline" style={{ opacity: offline }}> offline.</h1>
-                  <h1 className="online" style={{ opacity: online }}>online.</h1>
-                  <h1 className="like" style={{ opacity: like }}>anyway you like.</h1>
-                </div>
+      <section className="hero-bg">
+        <div className="hero">
+          <div className="text-hero">
+            <div className="head-hero">
+              <h1>
+                Search online buy</h1>
+              <div className="relative-text">
+                <h1 className="offline" style={{ opacity: offline }}> offline.</h1>
+                <h1 className="online" style={{ opacity: online }}>online.</h1>
+                <h1 className="like" style={{ opacity: like }}>anyway you like.</h1>
               </div>
-              <p>
-                Now search online what you want and buy offline as you like.
-              </p>
-              <a
-                href="https://play.google.com/store/apps"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Join Early Access
-              </a>
-              <ul>
-                <li>
-                  <img src={GooglePlay} alt="" />
-                </li>
-                <li>
-                  <img src={AppleStore} alt="" />
-                </li>
-              </ul>
             </div>
-            <div className="hero-img">
-              <img src={womanOffline} alt="" className="online" style={{ opacity: online }} />
-              <img src={womanOnline} alt="" className="offline" style={{ opacity: offline }} />
-              <img src={womanLike} alt="" className="like" style={{ opacity: like }} />
-            </div>
+            <p>
+              Now search online what you want and buy offline as you like.
+            </p>
+            <a
+              href="https://play.google.com/store/apps"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Join Early Access
+            </a>
+            <ul>
+              <li>
+                <img src={GooglePlay} alt="" />
+              </li>
+              <li>
+                <img src={AppleStore} alt="" />
+              </li>
+            </ul>
+          </div>
+          <div className="hero-img">
+            <img src={womanOffline} alt="" className="online" style={{ opacity: online }} />
+            <img src={womanOnline} alt="" className="offline" style={{ opacity: offline }} />
+            <img src={womanLike} alt="" className="like" style={{ opacity: like }} />
           </div>
         </div>
         <div className="stats">
