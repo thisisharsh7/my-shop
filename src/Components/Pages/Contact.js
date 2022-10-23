976f53f12176ac7200dc801df9a438831c87f2c3
Harsh Kumar <9u.harsh@gmail.com>
2022-10-24 01:07:49 +0530

    added form send to email for contanct js

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import phoneBg from '../Images/retroPhone.png';
 import phone from '../Images/phone.png';
 import locat from '../Images/locat.png';
 import msg from '../Images/msg.png';
+import emailjs from '@emailjs/browser';
 import { motion } from 'framer-motion';
 
 export default function Contact() {
+    const form = useRef();
+    function sendForm(e) {
+        e.preventDefault();
+        emailjs.sendForm('service_u65dn0o', 'template_cwxksxg', form.current, 'kaobwBiU0Spgr3VIe').then((result) => {
+            console.log(result.text);
+            alert('message send');
+        }, (error) => {
+            alert('error');
+        });
+    }
     return (
         <motion.main
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            exit={{ opacity: 0}}
+            exit={{ opacity: 0 }}
         >
             <section className='contact'>
                 <div className='contact-form'>
@@ -18,11 +29,11 @@ export default function Contact() {
                         <h1>Get In Touch</h1>
                         <p>We are here for you! How can we help?</p>
                     </div>
-                    <form>
+                    <form ref={form} onSubmit={sendForm}>
                         <input type="text" name="username" placeholder="Enter your name" />
                         <input type="email" name="useremail" placeholder="Enter your email address" />
                         <textarea className='area' name="usermessage" placeholder="Send us a message" />
-                        <input type="submit" name="submit" value="Submit"/>
+                        <input type="submit" name="submit" value="Submit" />
                     </form>
                 </div>
                 <div className='contactImg'>
