9e47fdc84596a2a9753e953c5be5329986e08e68
Harsh Kumar <9u.harsh@gmail.com>
2022-11-07 19:23:08 +0530

    meta tag improvment

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -8,10 +8,19 @@ import Suggestion from '../Home/Suggestion';
 import { motion } from 'framer-motion';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { Helmet } from 'react-helmet-async';
 
 export default function Home() {
     return (
         <div className='page-body'>
+            <Helmet>
+                <title>MyShop - Search Online Buy Offline</title>
+                <meta
+                    name="description"
+                    content="With our new app, you can see all the products being sold in your neigborhood market from the comfort of your own home."
+                />
+                <link rel="canonical" href="/" />
+            </Helmet>
             <Header />
             <motion.main
                 initial={{ opacity: 0 }}
